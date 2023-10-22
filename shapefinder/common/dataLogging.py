448df47d796595887e355ad30b60eecb008d152e
Deborah Schrag <deborah_schrag@outlook.de>
2023-10-22 16:03:58 +0200

    Logging works

--- shapefinder/common/dataLogging.py
+++ shapefinder/common/dataLogging.py
@@ -3,13 +3,15 @@ import csv
 
 class logger():
     def __init__(self):
-        self.time  = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        pass
 
     def log_to_csv(self, colors, shape):
         with open('logdata.csv', 'a', newline='', encoding='UTF8') as f:
             writer = csv.writer(f)
             time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            writer.writerow([time, colors, shape])
+            
+            for i in range(len(shape)):
+                writer.writerow([time, colors[i], shape[i]])
 
         
 

fecc405e335c0a8225f594d908c2a74b573ad49e
Lukas Marti <lukas.marti@stud.fhgr.ch>
2023-10-28 21:43:25 +0200

    finished code cleaning and added Logdata path selection functionality.

--- shapefinder/common/shape_detector.py
+++ shapefinder/common/shape_detector.py
@@ -2,10 +2,20 @@ import cv2
 
 
 class ShapeDetector:
+    """Detects shape in image with monotone background
+    """
     def __init__(self):
         pass
 
     def get_contours(self, image):
+        """Finds all the contours in the image
+
+        Args:
+            image: Input image to find contours in
+
+        Returns:
+            contours: Array containing all the contours, one shape per element
+        """
         grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, threshold_image = cv2.threshold(grayscale_image, 210, 255, 0)
         contours, _ = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -13,6 +23,15 @@ class ShapeDetector:
         return contours
 
     def evaluate_contours(self, contours):
+        """Analyses contours and puts them into different shape categories
+
+        Args:
+            contours: Array containing shape contours, one shape per element
+
+        Returns:
+            shapes: Array containing corresponding shape names to the contours
+            coordinates: Origin coordinates for all the shapes
+        """
         shapes = []
         coordinates = []
 
@@ -38,5 +57,4 @@ class ShapeDetector:
             shapes.append(shape)
             coordinates.append(coords)
 
-        # print("urgodman shapes: ", shapes)
         return shapes, coordinates
--- shapefinder/run/find_shapes_image.py
+++ shapefinder/run/find_shapes_image.py
@@ -9,13 +9,24 @@ from common.rt_visualization import Visualize
 from common.data_logging import Logger
 from common.shape_detector import ShapeDetector
 
+
+# Loading color ranges
+COLOR_RANGES = [
+        ["red", 0, 41],
+        ["yellow", 41, 70],
+        ["green", 71, 180],
+        ["blue", 181, 280],
+        ["purple", 260, 360],
+    ]
+
 # Creating Class Object
 IMAGE_PATH = "shapefinder/common/sample_image.JPG"
+LOG_PATH = "logs/logdata.csv"
 image = cv2.imread(IMAGE_PATH)
-color_det = ColorDetector()
+color_det = ColorDetector(COLOR_RANGES)
 shape = ShapeDetector()
 vis = Visualize(cv2.FONT_HERSHEY_PLAIN, (0, 0, 0))
-log = Logger()
+log = Logger(LOG_PATH)
 
 # Calling Class Functions
 contours = shape.get_contours(image)

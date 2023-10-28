5c212de88ddb049e199e40b0b1d0bbe7db733714
Lukas Marti <lukas.marti@stud.fhgr.ch>
2023-10-28 17:59:51 +0200

    Started code linting and improved structure

--- shapefinder/common/data_logging.py
+++ shapefinder/common/data_logging.py
@@ -1,18 +1,15 @@
 from datetime import datetime
 import csv
 
-class logger():
+
+class Logger:
     def __init__(self):
         pass
 
     def log_to_csv(self, colors, shape):
-        with open('logdata.csv', 'a', newline='', encoding='UTF8') as f:
+        with open("logdata.csv", "a", newline="", encoding="UTF8") as f:
             writer = csv.writer(f)
             time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            
+
             for i in range(len(shape)):
                 writer.writerow([time, colors[i], shape[i]])
-
-        
-
-
